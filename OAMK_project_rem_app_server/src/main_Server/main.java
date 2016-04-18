9e4ba5e884c6ac029c10d506b6414d31192fd803
kassajuraj <kassajuraj@gmail.com>
2016-04-18 15:36:05 +0300

    first upload main class

---
+++ OAMK_project_rem_app_server/src/main_Server/main.java
@@ -0,0 +1,19 @@
+package main_Server;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author juraj
+ *
+ */
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	server s = new server();
+	//	s.makeEasyGuiForServer();
+	//	s.runServer();
+
+	}
+
+}
